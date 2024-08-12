95da88e7489bc068a7282ca30977406ecb3678bc
staruvu2 <115789726+staruvu2@users.noreply.github.com>
2024-08-12 11:41:01 +0530

    Create sf case (#12)
    * this feature is to creat a case in SF
    
    * this feature is to creat a case in SFv2
    
    * create sf case fix v3
    
    * this feature is to create a case in SF v4
    
    * this feature is to create a case in SFv6

--- plugin-flex-ts-template-v2/src/feature-library/create-sf-case/flex-hooks/events/create-sf-case.ts
+++ plugin-flex-ts-template-v2/src/feature-library/create-sf-case/flex-hooks/events/create-sf-case.ts
@@ -10,42 +10,15 @@ import {
     screenPop,
     updateSfTicket,
   } from '../../utils/salesforcehelper';
-  import {createSfTicket2,
+  import {
+    createSfTicket2,
     createSfTask2,
     screenPop2,
-    updateSfTicket2 } from '../../utils/salesforcehelper2';
+    updateSfTicket2,
+  } from '../../utils/salesforcehelper2';//Sunil
+  
 import { Task } from 'types/task-router';
   //C:\Twilio-instancemerge-plugins\plugin-templatebased\twilio-flex-template-testv2\twilio-flex-template-test3-main\plugin-flex-ts-template-v2\src\feature-library\create-sf-case\utils
-
-//export const actionEvent = FlexActionEvent.before;
-//export const actionName = FlexAction.StartOutboundCall;
-const sfdcBaseUrl = window.location.ancestorOrigins[0]
-/*   
-*********SF Related code Start******* 
-if (!isSalesForce(sfdcBaseUrl)) {
-    console.log(
-    'Not initializing Salesforce since this instance has been launched independently...'
-    )
-    return
-}
-let salesforcepayload
-var sfClicklistener = function (payload) {
-    console.log('salesforce payload data onclick is ' + payload)
-    salesforcepayload = payload
-}
-
-const sfApiUrl = `${sfdcBaseUrl}/support/api/53.0/lightning/opencti_min.js`
-await loadScript(sfApiUrl)
-if (!window.sforce) {
-    console.log('Saleforce cannot be found')
-    return
-} else {
-    console.log('Adding salesforce listener..')
-    window.sforce.opencti.onClickToDial({ listener: sfClicklistener })
-}
-*********SF Related code END*******
-*/
-
 
 export const eventName = FlexEvent.taskAccepted;
 export const eventHook = function createCaseAfterTaskAcceptance(
@@ -57,6 +30,6 @@ export const eventHook = function createCaseAfterTaskAcceptance(
  console.log('Sunil- Task accepted-agent answered call----');
  console.log('Sunil-- task attributes--'+JSON.stringify(task.attributes));
     if (task.attributes.sfcontactid && task.attributes.sfcontactid !== ''  ) {
-        createSfTicket2(task)
+      createSfTicket2(task);
     };
 }
