a988f8f08021da02c3f07cb7f3d1cdb9822cf014
staruvu2 <115789726+staruvu2@users.noreply.github.com>
2024-08-14 09:49:08 +0530

    Create sf case (#22)
    * this feature is to creat a case in SF
    
    * this feature is to creat a case in SFv2
    
    * create sf case fix v3
    
    * this feature is to create a case in SF v4
    
    * this feature is to create a case in SFv6
    
    * this feature is to create a case in SFv7
    
    * create sf casetest v9
    
    * test v10
    
    * update Ticket attributes test v1
    
    * ticket id update test v3
    
    * test
    
    * test
    
    * Test
    
    * ticket updat v9
    
    * tst

--- plugin-flex-ts-template-v2/src/feature-library/create-sf-case/flex-hooks/events/create-sf-case.ts
+++ plugin-flex-ts-template-v2/src/feature-library/create-sf-case/flex-hooks/events/create-sf-case.ts
@@ -2,6 +2,11 @@ import * as Flex from '@twilio/flex-ui';
 import { isSalesForce } from '../../utils/salesforce';
 import { loadScript } from '../../utils/load-script'
 import { FlexEvent } from '../../../../types/feature-loader';
+
+/*import { ITask, Manager, Notifications } from '@twilio/flex-ui';
+const manager = Manager.getInstance();
+const workerLocation = manager.workerClient?.attributes?.location;*/
+
 //import TaskRouterService from '../../../../utils/serverless/TaskRouter/TaskRouterService';
 //TaskRouterService.updateTaskAttributes(taskSid, newAttribute);
 import {
@@ -23,7 +28,36 @@ export const eventHook = function createCaseAfterTaskAcceptance(
   // your code here
  console.log('Sunil- Task accepted-agent answered call----');
  console.log('Sunil-- task attributes--'+JSON.stringify(task.attributes));
+ console.log('Sunil- Task accepted-agent answered call taskSid----'+task.taskSid);
+ console.log('Sunil- Task accepted-agent answered call worker attributes----'+JSON.stringify(manager.workerClient?.attributes));
+ //console.log('worker attributes'+Flex.Manager.workerClient.attributes.userid);
     if (task.attributes.sfcontactid && task.attributes.sfcontactid !== ''  ) {
       createSfTicket(task);
     };
+
+    /*
+      if (task.attributes.sfcontactid && task.attributes.sfcontactid !== ''  ) {
+        if (task.attributes.ticketId && task.attributes.ticketId !== '') {
+          if (acceptedReservation._worker.attributes.userId) {
+            updateSfTicket(task.attributes.ticketId,acceptedReservation._worker.attributes.userId)
+            screenPop(acceptedReservation.task.attributes.ticketId)
+          }else{
+            console.log('Cannnot update ticket owner ID. Worker attributes missing userId.')
+          }
+        }else {
+            //No Existing Ticket Associated with Task
+            console.log('No existing Ticket in SF')
+            screenPop(acceptedReservation.task.attributes.sfcontactid)
+            createSfTicket(acceptedReservation)
+            createSfTask(acceptedReservation)
+            //searchAndScreenPop(acceptedReservation.task.attributes.did);
+          }
+      }else {
+          //No SF Contact ID
+          createSfTicket(acceptedReservation)//as per ESW-1739 added creation of tickets for un recognized callers//For Italy, no auto creation of tickets in case of caller not recognized
+          console.log('acceptedReservation else condition passed open lead---')
+          //screenPop();
+      }
+      
+    */
 }
