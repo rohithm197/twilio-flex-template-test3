ab832a3f818176bcba3800751bceed0afa6ccdf1
staruvu2 <staruvu@aligntech.com>
2024-09-23 14:12:49 +0530

    dynamic caller id commit -v2

--- plugin-flex-ts-template-v2/src/feature-library/caller-id/flex-hooks/actions/StartOutboundCall.ts
+++ plugin-flex-ts-template-v2/src/feature-library/caller-id/flex-hooks/actions/StartOutboundCall.ts
@@ -3,14 +3,28 @@ import * as Flex from '@twilio/flex-ui';
 import AppState from '../../../../types/manager/AppState';
 import { FlexActionEvent, FlexAction } from '../../../../types/feature-loader';
 import { reduxNamespace } from '../../../../utils/state';
+import {callerIdList} from '../../config'; //get the callerIds json/list from config
 
 export const actionEvent = FlexActionEvent.before;
 export const actionName = FlexAction.StartOutboundCall;
 export const actionHook = function applySelectedCallerIdForDialedNumbers(flex: typeof Flex, manager: Flex.Manager) {
   flex.Actions.addListener(`${actionEvent}${actionName}`, async (payload, _abortFunction) => {
     const state = manager.store.getState() as AppState;
-    const { selectedCallerId } = state[reduxNamespace].outboundCallerIdSelector;
 
-    if (!payload.callerId && selectedCallerId) payload.callerId = selectedCallerId;
+    //const { selectedCallerId } = state[reduxNamespace].outboundCallerIdSelector;    
+    //if (!payload.callerId && selectedCallerId) payload.callerId = selectedCallerId;
+
+    //Fetch loggedIn workers location
+    const loggedInWorkerLocation = manager.workerClient?.attributes.location;
+
+    //Define the callerId based on the workers location
+    const dynamicCallerId = callerIdList[loggedInWorkerLocation].phoneNumber;
+    
+    //Define the QueueSid based on the workers location
+    const dynamicQueueSid = callerIdList[loggedInWorkerLocation].queueSid;
+
+    payload.callerId = dynamicCallerId;//Override the caller Id in the payload
+    payload.queueSid = dynamicQueueSid;//Override the QueueSid in the payload
+
   });
 };
--- plugin-flex-ts-template-v2/src/feature-library/caller-id/flex-hooks/components/OutboundDialerPanel.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/caller-id/flex-hooks/components/OutboundDialerPanel.tsx
@@ -2,10 +2,17 @@ import * as Flex from '@twilio/flex-ui';
 
 import { FlexComponent } from '../../../../types/feature-loader/FlexComponent';
 import OutboundCallerIDSelector from '../../custom-components/OutboundCallerIDSelector';
+import OutboundQueueIDSelector from '../../custom-components/OutboundQueueIDSelector';
 
 export const componentName = FlexComponent.OutboundDialerPanel;
 export const componentHook = function addOutboundCallerIdSelectorToDialerPanel(flex: typeof Flex) {
   flex.OutboundDialerPanel.Content.add(<OutboundCallerIDSelector key="outbound-callerid-selector" />, {
     sortOrder: 1,
   });
+  /* use it for queue selector queue-select-caption */
+  flex.OutboundDialerPanel.Content.add(<OutboundQueueIDSelector key="outbound-queueid-selector" />, {
+    sortOrder: 1,
+  });
+  flex.OutboundDialerPanel.Content.remove('queue-select-caption');
+  flex.OutboundDialerPanel.Content.remove('queue-select');
 };
