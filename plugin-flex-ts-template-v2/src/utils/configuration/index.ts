736f45fec4eef0a4912763ed0ae543ff9b14ff20
RohithM <rmaleti@aligntech.com>
2025-11-07 12:01:19 +0530

    'dach'

--- plugin-flex-ts-template-v2/src/utils/configuration/index.ts
+++ plugin-flex-ts-template-v2/src/utils/configuration/index.ts
@@ -51,7 +51,7 @@ export const getFeatureFlags = () => {
       teams = selectedTeams ? teamList[selectedTeams] : [];
       queuesStatsList = workerQueues
         ? location === 'plhub'
-          ? [...(queuesList?.IB || []), ...(queuesList?.UK || [])]
+          ? [...(queuesList?.IB || []), ...(queuesList?.UK || []),...(queuesList?.DACH || [])]
           : queuesList[workerQueues]
         : [];
     }
